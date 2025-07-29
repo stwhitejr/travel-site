ac356bdb886f075534467ddad3ae5a06c6ae2792
Steve White <stwhitejr@gmail.com>
2025-07-29 10:44:14 -0400

    fix layout

--- src/app/category/layout.tsx
+++ src/app/category/layout.tsx
@@ -3,7 +3,7 @@ import {ReactNode} from 'react';
 
 export default function CategoryLayout(props: {children: ReactNode}) {
   return (
-    <div className="flex flex-col md:h-full md:overflow-y-hidden">
+    <div className="flex flex-col h-full md:overflow-y-hidden">
       <Header />
       <div className="flex-2 md:h-full relative md:overflow-y-auto">
         {props.children}
